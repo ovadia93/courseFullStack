dcd4f6a8c4f73ba36e50f27eb7b26210303265e1
Ben Ovadia <ovadia93@gmail.com>
2022-07-29 12:00:26 +0100

    init

--- Lesson 35/index.php
+++ Lesson 35/index.php
@@ -1,11 +1,3 @@
-
-<?php
-     //for ($i = 0; $i < 50; $i++) {
-     //echo "<p>המספר הנוכחי הוא $i</p>";
-     //}
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,12 +7,22 @@
         <title>Form</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="jquery.js"></script>
+        <script src="script.js"></script>
         <link rel="stylesheet" href="style.css">
     </head>
+
     <body>
-        <h1>שלום <?= $_POST['firstName'] ?></h1>
-        <p><b>שם פרטי: </b> <?= $_POST['firstName'] ?></p>
-        <p><b>שם משפחה: </b> <?= $_POST['lastName'] ?></p>
-        <p><b>סיסמה: </b> <?= $_POST['password'] ?></p>
+        <form action="login.php" method="post">
+            <h1>התחברות</h1>
+
+            <label>שם משתמש:</label>
+            <input type="text" class="form-control" required name="userName">
+
+            <label>סיסמה</label>
+            <input type="password" class="form-control" required name="password">
+
+            <button class="btn btn-success" type="submit">שלח</button>
+        </form>
     </body>
 </html>
