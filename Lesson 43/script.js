518f412fe2e32688eafbb11a94f53455cd9beeab
Ovadia93 <you@example.com>
2022-09-11 15:40:21 +0100

    init

--- Lesson 43/script.js
+++ Lesson 43/script.js
@@ -1,69 +1,30 @@
 $(function() {
-    createBoard();
+    $.get("students.txt", function(res) {
+        const classes = JSON.parse(res);
+
+        classes.forEach(c => {
+            $('select').append(`<option value="${c.id}">כיתה ${c.name}</option>`);
+        });
+
+        $('select').change(function() {
+            $('#students').html("");
+
+            const classId = $(this).val();
+            const temp = $("table template").html();
+            const c = classes.find(x => x.id == classId);
+
+            c.students.forEach((x, i) => {
+                const elem = $(temp);
+
+                elem.find('#numbering').html(i + 1);
+                elem.find('#fullName').html(x.fullName);
+                elem.find('#personId').html(x.personId);
+                elem.find('#phone').html(x.phone);
+                elem.find('#email').html(x.email);
+                elem.find('#grades').html(x.grades.join(', '));
+
+                $('#students').append(elem);
+            });
+        });
+    });
 });
-
-function createBoard() {
-    const numbers = [];
-
-    for (let i = 1; i <= 16; i++)
-        numbers.push(i);
-
-    $('td').each(function(i) {
-        const rand = Math.floor(Math.random() * numbers.length);
-        const num = numbers[rand];
-        numbers.splice(rand, 1);
-
-        $(this).attr('id', i);
-
-        if (num == 16) {
-            $(this).html(`<div></div>`);
-        } else {
-            $(this).html(`<div>${num}</div>`);
-        }
-    });
-
-    $('td').click(function() {
-        const index = +$(this).attr('id');
-        const next = $(this).next();
-        const prev = $(this).prev();
-        const top = $('td').eq(index - 4);
-        const bottom = $('td').eq(index + 4);
-
-        const source = $(this).html();
-        let target;
-
-        if (!prev.text() && prev.length) {
-            target = prev.html();
-            prev.html(source);
-        } else if (!next.text() && next.length) {
-            target = next.html();
-            next.html(source);
-        } else if (!top.text() && top.length) {
-            target = top.html();
-            top.html(source);
-        } else if (!bottom.text() && bottom.length) {
-            target = bottom.html();
-            bottom.html(source);
-        }
-
-        if (target) {
-            $(this).html(target);
-        }
-    });
-}
-
-function isCompleted() {
-    let isOrder = true;
-
-    $('td').each(function(i) {
-        const val = $(this).text();
-        const num = i + 1;
-
-        if (val != num && num != 16) {
-            isOrder = false;
-            return;
-        }
-    });
-
-    return isOrder;
-}
