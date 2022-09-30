c1ec49929d52484e01d03bce4da1c67cb612cfbf
Ovadia93 <you@example.com>
2022-09-30 12:16:30 +0100

    init

--- Practice/script.js
+++ Practice/script.js
@@ -1,11 +1,13 @@
-function ChangeColor {
+function changeColor() {
 
-
-    document.getElementById("ChangeColor").style.color = "orange";
+    document.getElementById("changeColor").style.color = "orange";
     document.getElementById("h2").style.background = "black";
 
     document.getElementById("h3").style.color = "grey";
 
     document.getElementById("h4").style.color = "yellow";
     document.getElementById("h4").style.background = "green";
+
+    console.log(changeColor)
+
 }
