701cbf3e70ae6d135888c96d280366f29144f8b7
Ovadia93 <you@example.com>
2022-10-30 19:07:14 +0000

    init

---
+++ helloAngular/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListComponent } from './list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
