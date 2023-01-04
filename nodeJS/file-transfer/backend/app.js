6a1229f75838d884e64279c4ed74347653ea10cb
Ovadia93 <you@example.com>
2023-01-04 16:33:30 +0000

    init

--- nodeJS/file-transfer/backend/app.js
+++ nodeJS/file-transfer/backend/app.js
@@ -34,13 +34,16 @@ app.post('/gallery/upload', (req, res) => {
             }
             
             res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(`<meta charset="UTF-8">`);
+            res.write(`<h1>התמונה עלתה בהצלחה</h1>`);
             res.write(`<img src="http://localhost:770/gallery/image/${myFile.originalFilename}" width="500">`);
 
             res.end();
         });
     });
-})
+});
 
 app.get('/gallery/image/:imageName', (req, res) => {
+    // __dirname = מביא את הניתוב של התיקייה שבה נמצא הקובץ הנוכחי
     res.sendFile(path.resolve(`${__dirname}/images/${req.params.imageName}`));
-})
+});
